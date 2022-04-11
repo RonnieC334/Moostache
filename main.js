7a87757e8074c80b2dd42c287b53594a3c6b68aa
RonnieC334 <91676482+RonnieC334@users.noreply.github.com>
2022-04-11 19:23:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+function preload() {
+
+}
+
+function setup() {
+    canvas = createCanvas(400,400);
+    canvas.center();
+    canvas.position(489,200)
+}
+
+function draw() {
+
+}
+
+function takesnap() {
+    save("clownnose.png");
+    
+}
